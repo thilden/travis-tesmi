3b7f3278d02f9ba1ce0adbe3440e68e2f29d5435
Timo Hildén <timo@codeflow.fi>
2020-08-24 14:55:22 +0300

    Added tests

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require("assert")
+describe("Array", () => {
+    describe("#indexOf()", () => {
+        it("should return -1 when value is not present... oujea", () => {
+            assert.equal([1, 2, 3].indexOf(4), -1)
+        })
+    })
+})
+
